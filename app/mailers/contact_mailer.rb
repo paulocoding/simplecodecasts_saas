12bdc4a51a99be431b772d3a0ae4c2a461c979e7
Paulo Silva <paulo.silva.webdev@gmail.com>
2015-11-12 10:39:03 +0000

    added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ActionMailer::Base
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact form Message')
+  end
+end
