63fe13b9831a37ad7cf40d58203a7198948b7cd5
Paulo Silva <paulo.silva.webdev@gmail.com>
2015-11-17 12:25:10 +0000

    added profile filters

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
@@ -14,7 +16,6 @@ class ProfilesController < ApplicationController
     else
       render action: :new
     end
-      
   end
   
   def edit
@@ -22,8 +23,23 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+    if  @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id]) 
+    else
+      render action: :edit
+    end
+  end
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find(params[:user_id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
